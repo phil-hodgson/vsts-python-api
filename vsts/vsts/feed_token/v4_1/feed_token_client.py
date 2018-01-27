4384218bd680a2496797059ffeff1f87fe80d5f6
Salvador Melendez-Delgado <sameln@microsoft.com>
2018-01-26 21:11:09 -0800

    Adding area identifier of FeedToken client.

--- vsts/vsts/feed_token/v4_1/feed_token_client.py
+++ vsts/vsts/feed_token/v4_1/feed_token_client.py
@@ -23,7 +23,7 @@ class FeedTokenClient(VssClient):
         self._serialize = Serializer(client_models)
         self._deserialize = Deserializer(client_models)
 
-    resource_area_identifier = None
+    resource_area_identifier = 'cdeb6c7d-6b25-4d6f-b664-c2e3ede202e8'
 
     def get_personal_access_token(self, feed_name=None):
         """GetPersonalAccessToken.
